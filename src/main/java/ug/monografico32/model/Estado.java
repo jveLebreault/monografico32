263c16dc17660dd9fc6fe3b7a933c9151ca216c1
jveLebreault <joseevargasl@gmail.com>
2016-10-29 18:08:10 -0400

    Clase Estado creado, vista /home creada

---
+++ src/main/java/ug/monografico32/model/Estado.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ug.monografico32.model;
+
+/**
+ *
+ * @author Administrador
+ */
+public enum Estado {
+    ACTIVO,
+    INACTIVO
+}
