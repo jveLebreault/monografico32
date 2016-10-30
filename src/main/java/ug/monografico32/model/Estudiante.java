4d3e5bfb95995176bad08226bc8aecd0b74cdeba
jveLebreault <joseevargasl@gmail.com>
2016-10-29 23:31:33 -0400

    Curso class added

--- src/main/java/ug/monografico32/model/Estudiante.java
+++ src/main/java/ug/monografico32/model/Estudiante.java
@@ -1,7 +1,12 @@
 package ug.monografico32.model;
 
+import javax.validation.constraints.Size;
+
 /**
  * Created by Jose Elias on 25/10/2016.
  */
 public class Estudiante extends Persona{
+    
+    @Size(min = 1, max =1)
+    private String seccion;
 }
