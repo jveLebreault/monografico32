c41cdec0d13b24f3bcb8fa46ec573161ed755019
jveLebreault <joseevargasl@gmail.com>
2016-12-03 14:58:51 -0400

    Create URLSignerWrapper with it corresponding test case

--- src/main/java/ug/monografico32/config/AppConfig.java
+++ src/main/java/ug/monografico32/config/AppConfig.java
@@ -7,20 +7,36 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Region;
+import java.net.URISyntaxException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import ug.monografico32.model.aws.URLSignerWrapper;
 
 /**
  * Created by Jose Elias on 24/10/2016.
  */
 @Configuration
+@PropertySource("classpath:/aws/cloudfront.properties")
 //@ComponentScan()
 public class AppConfig {
+    
+    @Autowired
+    private Environment env;
+    
+    @Bean
+    public URLSignerWrapper urlSignerWrapper() throws URISyntaxException{
+        return new URLSignerWrapper(env.getProperty("distributionDomain"),
+                                    env.getProperty("keypairId"),
+                                    env.getProperty("privateKeyFilePath"));
+    }
 
     @Bean
     public AWSCredentialsProvider classpathPropertiesFileCredentialsProvider(){
-        return new ClasspathPropertiesFileCredentialsProvider("aws/awscredentials.properties");
+        return new ClasspathPropertiesFileCredentialsProvider("aws/s3credentials.properties");
     }
 
     @Bean
--- src/test/java/ug/monografico32/model/AWSTest.java
+++ src/test/java/ug/monografico32/model/AWSTest.java
@@ -22,6 +22,7 @@ import java.security.spec.InvalidKeySpecException;
 import java.util.Date;
 
 import static org.junit.Assert.assertNotNull;
+import ug.monografico32.model.aws.URLSignerWrapper;
 
 /**
  * Created by Jose Elias on 01/12/2016.
@@ -33,25 +34,15 @@ public class AWSTest {
 
     @Autowired
     private AmazonS3 s3Client;
+    
+    @Autowired
+    private URLSignerWrapper urlSigner;
 
     @Test
     public void KeyURLTest() throws IOException, InvalidKeySpecException {
-        // the DNS name of your CloudFront distribution, or a registered alias
-        String distributionDomainName = "d3tel6qtlf2frl.cloudfront.net";
-// the private key you created in the AWS Management Console
-        File cloudFrontPrivateKeyFile = new File( "C:\\Users\\Jose Elias\\IdeaProjects\\monografico32\\src\\main\\resources\\aws\\spk-APKAI4TDCZZ35A5ORW3A.der" );
-// the unique ID assigned to your CloudFront key pair in the console
-        String cloudFrontKeyPairId = "APKAI4TDCZZ35A5ORW3A";
-        Date expirationDate = new Date(System.currentTimeMillis() + 60 * 1000);
-
-        String signedUrl = CloudFrontUrlSigner.getSignedURLWithCannedPolicy(
-                SignerUtils.Protocol.https,
-                distributionDomainName,
-                cloudFrontPrivateKeyFile,
-                "aws.jpg", // the resource path to our content
-                cloudFrontKeyPairId,
-                expirationDate);
-        System.out.println(signedUrl);
+       String signedUrl = urlSigner.getURLFor("aws.jpg");
+       System.out.println(signedUrl);
+       assertNotNull(signedUrl);
     }
 
     @Test
@@ -61,6 +52,7 @@ public class AWSTest {
 
         InputStream is = getClass().getResourceAsStream("/something.txt");
         ObjectMetadata metadata = new ObjectMetadata();
-        s3Client.putObject("monografico32", System.currentTimeMillis()+".txt",is, metadata);
+        s3Client.putObject("monografico32", System.currentTimeMillis()+".txt",
+                is, metadata);
     }
 }
