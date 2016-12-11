19e74603ac9aafd7a4171dd5263a91ba554e38e1
jveLebreault <joseevargasl@gmail.com>
2016-12-11 15:41:56 -0400

    Goddamit webflow

--- src/main/java/ug/monografico32/dao/EstudianteRepository.java
+++ src/main/java/ug/monografico32/dao/EstudianteRepository.java
@@ -1,7 +1,12 @@
 package ug.monografico32.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import ug.monografico32.model.Estudiante;
+
 /**
  * Created by Jose Elias on 07/12/2016.
  */
-public interface EstudianteRepository {
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long>{
+    
+    Estudiante findById(Long id);
 }
