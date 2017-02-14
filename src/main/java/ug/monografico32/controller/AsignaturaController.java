cfc68882240f37d2c75b3056e92cd64b8eec5ebc
jveLebreault <joseevargasl@gmail.com>
2017-02-14 07:31:46 -0400

    Changed the lang attribute of the html pages to es

---
+++ src/main/java/ug/monografico32/controller/AsignaturaController.java
@@ -0,0 +1,7 @@
+package ug.monografico32.controller;
+
+/**
+ * Created by Jose Elias on 14/02/2017.
+ */
+public class AsignaturaController {
+}
---
+++ src/main/java/ug/monografico32/dao/AsignaturaRepository.java
@@ -0,0 +1,7 @@
+package ug.monografico32.dao;
+
+/**
+ * Created by Jose Elias on 14/02/2017.
+ */
+public interface AsignaturaRepository {
+}
