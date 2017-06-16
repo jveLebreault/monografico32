b8ea1b1c20ff750bbf434fcac48a72269e2d1bed
jveLebreault <joseevargasl@gmail.com>
2017-06-15 23:24:16 -0400

    added PeriodoTest class

--- src/test/java/ug/monografico32/model/PeriodoTest.java
+++ src/test/java/ug/monografico32/model/PeriodoTest.java
@@ -1,7 +1,34 @@
 package ug.monografico32.model;
 
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import static org.junit.Assert.*;
+
 /**
  * Created by Jose Elias on 15/06/2017.
  */
 public class PeriodoTest {
+
+    Periodo p1;
+    Periodo p2;
+
+
+    public void periodoSetUp(){
+        Calendar now = Calendar.getInstance();
+        Date d1 = now.getTime();
+
+        now.add(Calendar.MONTH, 1);
+        Date d2 = now.getTime();
+
+        p1 = new Periodo(d1, d2);
+        p2 = new Periodo(d1, d2);
+    }
+
+    @Test
+    public void testForEquality(){
+        periodoSetUp();
+        assertTrue(p1.equals(p2));
+    }
 }
