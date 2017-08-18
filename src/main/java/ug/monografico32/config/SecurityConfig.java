ff5f012716cbb244aec0868b58130a8c1f232ee7
jveLebreault <joseevargasl@gmail.com>
2017-08-17 22:01:52 -0400

    Added URL mapping for each of the existing roles

--- src/main/java/ug/monografico32/config/SecurityConfig.java
+++ src/main/java/ug/monografico32/config/SecurityConfig.java
@@ -12,26 +12,45 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.
-           inMemoryAuthentication().
-                withUser("admin").password("admin").roles("ADMIN").and().
-                withUser("estudiante").password("estudiante").roles("ESTUDIANTE").and().
-                withUser("docente").password("docente").roles("DOCENTE");
+    private static final String[] AUTH_URL = {"/asignatura/\\d+",
+                            "/curso/\\d+","/curso/\\d+/estudiantes",
+                            "/estudiante/\\d+","/estudiante/\\d+/cursos",
+                            "/horario/clase/\\d+",
+                            "/","/home"};
 
-    }
+    private static final String[] ADMIN_URL = {"/asignatura/**", "/curso/**", "/docente/**",
+                                    "/estudiante/**", "/horario/**", "/periodo/**"};
+
+    private static final String[] DOCENTE_URL = {"/curso/all\\?encargado=\\d+", "/docente/\\d+",
+                                            "/horario/clase\\?instructor=\\d+",
+                                            "/periodo/seleccionar"};
+
+    private static final String[] ESTUDIANTE_URL = {};
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.
             authorizeRequests().
-                antMatchers("/resources/**", "/login**").permitAll().and().
+                antMatchers("/resources/**", "/login**").permitAll().
+                regexMatchers(AUTH_URL).authenticated().
+                regexMatchers(DOCENTE_URL).hasRole("DOCENTE").
+                antMatchers(ADMIN_URL).hasRole("ADMIN").
+            and().
             authorizeRequests().
-                anyRequest().authenticated().and().
+                anyRequest().authenticated().
+            and().
                 formLogin().
-                    loginPage("/login").
-                    //permitAll().
-                    defaultSuccessUrl("/home");
+                loginPage("/login").
+                defaultSuccessUrl("/home");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.
+                inMemoryAuthentication().
+                withUser("admin").password("admin").roles("ADMIN").and().
+                withUser("estudiante").password("estudiante").roles("ESTUDIANTE").and().
+                withUser("docente").password("docente").roles("DOCENTE");
+
     }
 }
