894e90f37448c11ed77d9fe8b3628550ce887cdd
jveLebreault <joseevargasl@gmail.com>
2017-09-02 13:22:55 -0400

    Using authorities insted of roles, changed some rules on url permissions, now correctly retrieving users from the database

--- src/main/java/ug/monografico32/config/SecurityConfig.java
+++ src/main/java/ug/monografico32/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import ug.monografico32.service.UserService;
 
 /**
  * Created by Jose Elias on 05/08/2017.
@@ -12,14 +13,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String[] PUBLIC_URL = {"/docente/agregar/**", "/estudiante/agregar*",
+                                                "/admin/agregar/**",
+                                                "/resources/**", "/login**"};
+
     private static final String[] AUTH_URL = {"/asignatura/\\d+",
                             "/curso/\\d+","/curso/\\d+/estudiantes",
                             "/estudiante/\\d+","/estudiante/\\d+/cursos",
                             "/horario/clase/\\d+",
                             "/","/home"};
 
-    private static final String[] ADMIN_URL = {"/asignatura/**", "/curso/**", "/docente/**",
-                                    "/estudiante/**", "/horario/**", "/periodo/**"};
+    private static final String[] ADMIN_URL = {"**/**", "/**", "/docente/**"};
 
     private static final String[] DOCENTE_URL = {"/curso/all\\?encargado=\\d+", "/docente/\\d+",
                                             "/horario/clase\\?instructor=\\d+",
@@ -27,14 +31,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] ESTUDIANTE_URL = {};
 
+    @Autowired
+    private UserService userService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.
             authorizeRequests().
-                antMatchers("/resources/**", "/login**").permitAll().
+                antMatchers(PUBLIC_URL).permitAll().
                 regexMatchers(AUTH_URL).authenticated().
-                regexMatchers(DOCENTE_URL).hasRole("DOCENTE").
-                antMatchers(ADMIN_URL).hasRole("ADMIN").
+                antMatchers(ADMIN_URL).hasAuthority("ADMINISTRADOR").
+                regexMatchers(DOCENTE_URL).hasAuthority("DOCENTE").
             and().
             authorizeRequests().
                 anyRequest().authenticated().
@@ -46,11 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.
-                inMemoryAuthentication().
-                withUser("admin").password("admin").roles("ADMIN").and().
-                withUser("estudiante").password("estudiante").roles("ESTUDIANTE").and().
-                withUser("docente").password("docente").roles("DOCENTE");
 
+        auth.userDetailsService(userService);
     }
 }
