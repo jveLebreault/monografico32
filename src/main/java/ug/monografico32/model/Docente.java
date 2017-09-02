ec6f6cc7cfc197085691acb9accafcef9d551b11
jveLebreault <joseevargasl@gmail.com>
2017-09-02 13:12:09 -0400

    Removed personal legal and added numerocedula property to Docente and Tutor

--- src/main/java/ug/monografico32/model/Docente.java
+++ src/main/java/ug/monografico32/model/Docente.java
@@ -3,33 +3,53 @@ package ug.monografico32.model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * Created by Jose Elias on 25/10/2016.
  */
 @Entity
-public class Docente extends PersonaLegal implements Serializable{
+public class Docente extends Usuario implements Serializable{
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "docenteEncargado")
     private List<Curso> cursosEncargado;
     {cursosEncargado = new ArrayList<>();}
 
+    @Column(unique = true)
+    @NotNull
+    @Size(min = 11, max = 13)
+    private String numeroCedula;
+
     public Docente(){
         super();
+        this.addAuthority(Roles.DOCENTE);
     }
     
     public Docente(String nombres, String apellidos, String cedula){
-        super(nombres, apellidos, cedula);
+        super();
+        this.setNombres(nombres);
+        this.setApellidos(apellidos);
+        this.numeroCedula = cedula;
     }
 
-    public void setCursoEncargado(List<Curso> curso){
-        this.cursosEncargado = curso;
-    }
-
-    public List<Curso> getCursoEncargado(){
+    public List<Curso> getCursosEncargado() {
         return cursosEncargado;
+    }
+
+    public void setCursosEncargado(List<Curso> cursosEncargado) {
+        this.cursosEncargado = cursosEncargado;
+    }
+
+    public String getNumeroCedula() {
+        return numeroCedula;
+    }
+
+    public void setNumeroCedula(String numeroCedula) {
+        this.numeroCedula = numeroCedula;
     }
 }
--- src/main/java/ug/monografico32/model/Tutor.java
+++ src/main/java/ug/monografico32/model/Tutor.java
@@ -1,32 +1,64 @@
 package ug.monografico32.model;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * Created by Jose Elias on 24/11/2016.
  */
 @Entity
-public class Tutor extends PersonaLegal implements Serializable{
+public class Tutor extends Persona implements Serializable{
 
-    @OneToOne( cascade = CascadeType.ALL ,orphanRemoval = true)
-    private AmazonS3Document cedula;
+    @Column(unique = true)
+    @NotNull
+    @Size(min = 11, max = 13)
+    private String numeroCedula;
 
     public Tutor(){
         super();
     }
 
     public Tutor(String nombres, String apellidos, String nroCedula){
-        super(nombres, apellidos,nroCedula);
+        super(nombres, apellidos);
+        this.numeroCedula = nroCedula;
     }
 
     public void setCedula(AmazonS3Document cedula){
-        this.cedula = cedula;
+        this.agregarDocumento(cedula);
     }
 
     public AmazonS3Document getCedula(){
-        return cedula;
+        return this.getDocuments().
+                stream().filter( doc -> doc.getDocumentType().equals(DocumentType.CEDULA)).
+                findFirst().get();
+    }
+
+    public String getNumeroCedula() {
+        return numeroCedula;
+    }
+
+    public void setNumeroCedula(String numeroCedula) {
+        this.numeroCedula = numeroCedula;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tutor tutor = (Tutor) o;
+
+        return numeroCedula.equals(tutor.numeroCedula);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(numeroCedula);
     }
 }
