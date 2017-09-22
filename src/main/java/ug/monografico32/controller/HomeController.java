604b64701cebf8f64a41d24a0a26bf0bc6629009
jveLebreault <joseevargasl@gmail.com>
2017-09-21 20:54:57 -0400

    Added forwarding based on available roles

--- src/main/java/ug/monografico32/controller/HomeController.java
+++ src/main/java/ug/monografico32/controller/HomeController.java
@@ -5,12 +5,18 @@
  */
 package ug.monografico32.controller;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import ug.monografico32.model.Roles;
+import ug.monografico32.model.Usuario;
+
+import java.util.Collection;
 
 /**
  *
@@ -20,7 +26,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
 
     @RequestMapping( path = {"/", "/home"}, method = RequestMethod.GET)
-    public String home(){
+    public String home(@AuthenticationPrincipal Usuario usuario){
+        Collection<? extends GrantedAuthority> roles = usuario.getAuthorities();
+
+        if( roles.contains(Roles.ESTUDIANTE)){
+
+            return "forward:/estudiante/"+usuario.getId();
+        } else if(roles.contains(Roles.DOCENTE)){
+
+            return "forward:/docente/"+usuario.getId();
+        }
+
         return "home";
     }
 
