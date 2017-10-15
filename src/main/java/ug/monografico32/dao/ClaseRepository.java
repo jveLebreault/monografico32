2a86a804ebb0ef3022f88e8e78faf67f4779ffd8
jveLebreault <joseevargasl@gmail.com>
2017-10-15 11:16:13 -0400

    ClaseRepository now  deletes by Id and find and get asignaciones

--- src/main/java/ug/monografico32/dao/ClaseRepository.java
+++ src/main/java/ug/monografico32/dao/ClaseRepository.java
@@ -5,12 +5,16 @@ import java.util.stream.Stream;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import ug.monografico32.model.*;
 
 public interface ClaseRepository extends JpaRepository<Clase, Long>{
 
     @Query("SELECT c FROM Clase c LEFT JOIN FETCH c.sesiones WHERE c.id = ?1")
     Clase findAndFetchSesiones(Long id);
+
+    @Query("SELECT c FROM Clase c LEFT JOIN FETCH c.asignaciones WHERE c.id = ?1")
+    Clase findAndFetchAsignaciones(Long id);
 
     @Query("SELECT c FROM Clase c LEFT JOIN FETCH c.sesiones WHERE c.instructor.id = ?1")
     Stream<Clase> findByInstructorId(Long id);
@@ -26,4 +30,7 @@ public interface ClaseRepository extends JpaRepository<Clase, Long>{
     @Query("SELECT DISTINCT c FROM Clase c LEFT JOIN FETCH c.asignaciones a " +
             "LEFT JOIN FETCH a.calificaciones WHERE c.horario.curso = ?1 AND ?2 MEMBER OF a.asignados")
     List<Clase> findByCursoAndEstudianteFetchAsignaciones(Curso curso, Estudiante estudiante);
+
+    @Transactional
+    Long deleteById(Long id);
 }
